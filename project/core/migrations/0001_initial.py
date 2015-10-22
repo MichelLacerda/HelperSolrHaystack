a506b96e49671472d79a3a0af9f221c0a91c7565
Michel Lacerda <michellacerdasilva@gmail.com>
2015-10-22 18:34:55 -0200

    Initial

---
+++ project/core/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('pub_date', models.DateField(default=None)),
+                ('update_at', models.DateField(auto_now=True)),
+                ('author', models.ForeignKey(to='core.Author')),
+            ],
+        ),
+    ]
---
+++ project/core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        self.name
+
+
+class Book(models.Model):
+    author = models.ForeignKey(Author)
+    title = models.CharField(max_length=255)
+    pub_date = models.DateField(default=None)
+    update_at = models.DateField(auto_now=True)
+
+    def __unicode__(self):
+        return self.title
